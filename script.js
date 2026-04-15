2bc3b3ae0d754e2c12bee22e026dea423b562bad
Keyrisson <keyrisson.sibert@escola.pr.gov.br>
2026-04-15 02:25:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const vidros = {
+    "normal": {
+        nome: "vidro normal",
+        preco: "R$ 300,00",
+        descricao: "vidro tranparente nomal ",
+        imagem: "img/branco.png"
+    },
+}
