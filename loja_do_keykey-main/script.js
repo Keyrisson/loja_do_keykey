13360079c779b121784b80d78d69bd640069e024
Keyrisson <keyrisson.sibert@escola.pr.gov.br>
2026-04-22 11:42:53 -0300

    Delete loja_do_keykey-main directory

--- loja_do_keykey-main/script.js
+++
@@ -1,8 +0,0 @@
-const vidros = {
-    "normal": {
-        nome: "vidro normal",
-        preco: "R$ 300,00",
-        descricao: "vidro tranparente nomal ",
-        imagem: "img/branco.png"
-    },
-}
